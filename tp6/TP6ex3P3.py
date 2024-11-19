39c9b56a60626fa51c7f38e6780eef4cf2de5904
Samuel46446 <156199948+Samuel46446@users.noreply.github.com>
2024-11-19 14:45:56 +0100

    add nbTentative and start P4

--- tp6/TP6ex3P3.py
+++ tp6/TP6ex3P3.py
@@ -4,12 +4,16 @@ from random import*
 
 choixNbrOrdi=randint(1,100)
 repPlayer=0
+nbTentative=0
 
 if 1 <= choixNbrOrdi <= 100:
     while repPlayer != choixNbrOrdi:
         repPlayer=int(input("J'ai choisie un nombre entre 1 et 100, essaye de le trouver : "))
         if repPlayer > choixNbrOrdi:
             print("Plus petit")
+            nbTentative += 1
         elif repPlayer < choixNbrOrdi:
             print("Plus grand")
+            nbTentative += 1
 print("Bravo le nombre secret était ", choixNbrOrdi)
+print("Tu as réussis en ", nbTentative, " tentatives")
--- tp6/TP6ex3P4.py
+++ tp6/TP6ex3P4.py
@@ -1,3 +1,13 @@
 
 #Nombres premiers
 
+nbr=0
+
+while nbr != 0 and nbr >=100:
+    nbr = int(input("Entrez un nombre pour savoir s'il est premier : "))
+
+if (nbr >= 1 and nbr%nbr <= 0 and nbr%1 <= 0) or nbr == 2:
+    print("Votre nombre est premier !")
+else:
+    print("Votre nombre n'est pas premier !")
+
