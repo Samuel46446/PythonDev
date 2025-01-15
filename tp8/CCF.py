f22b0ddc8c33136e4be136026c8f501c09b2bdd1
Samuel46446 <156199948+Samuel46446@users.noreply.github.com>
2025-01-15 17:53:04 +0100

    corrected version of ccf

--- tp8/CCF.py
+++ tp8/CCF.py
@@ -1,14 +1,22 @@
 aux=0
 n=int(input("Saisir n val : "))
 tab=n*[0]
-
-for i in range(0, n-1):
-    tab[i]=int(input("Saisir val "+ str(i) +" du tableau"))
-for j in range(0, n-2):
-    for i in range(0, n - 2):
+cpt=0
+for i in range(0, n):
+    tab[i]=int(input("Saisir val "+ str(i) +" du tableau : "))
+tab1=[]
+j=0
+while tab1 != tab:
+    tab1=tab
+    print("j debut : ", tab)
+    for i in range(0, n - 1 - j):
         if tab[i] > tab[i+1]:
+            cpt=cpt+1
             aux=tab[i]
             tab[i]=tab[i+1]
             tab[i+1] = aux
-for i in range(0, n-1):
+            print("i fin : ", tab)
+    j=j+1
+for i in range(0, n):
     print(tab[i])
+print("Nombre de modif : ", cpt)
